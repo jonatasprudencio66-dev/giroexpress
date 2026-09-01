1dc1a114db2c75800dee20b2501e409325719c71
Cesar <cesar@gmail.com>
2026-09-01 16:26:49 -0300

    Atualiza para o tunnel young-jeans-appear

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "https://lovely-views-hunt.loca.lt/api";
+export const API_BASE = "git add .";
 
 export const api = axios.create({
   baseURL: API_BASE,
