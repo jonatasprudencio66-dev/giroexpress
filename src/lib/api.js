f8b0bce15760865d39a45438cb6231142d85655d
Cesar <cesar@gmail.com>
2026-09-02 08:36:37 -0300

    Atualiza url do tunel

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_BASE = "https://giroexpress-backend-dev.loca.lt/api";
+export const API_BASE = "https://stupid-queens-study.loca.ltgit add .";
 
 export const api = axios.create({
   baseURL: API_BASE,
