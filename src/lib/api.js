b172190a80ce9560e03805f54916aa94920d4bdb
Cesar <cesar@gmail.com>
2026-09-03 08:29:03 -0300

    Usa caminho relativo na API_BASE do axios

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const API_BASE = "https://giroexpress-9ufp.vercel.app/api";
+
+export const API_BASE = "/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
